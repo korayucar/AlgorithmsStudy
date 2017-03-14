1f9d6d9d2c04c5a7803b7e2c92a3abe85cda3557
Koray Ucar <kryucar@gmail.com>
2017-03-14 22:44:55 +0300

    fix flooding city test cases

--- src/test/java/xyz/korayucar/algorithmstudy/floodingcity/AbstractFloodingCityTest.java
+++ src/test/java/xyz/korayucar/algorithmstudy/floodingcity/AbstractFloodingCityTest.java
@@ -1,46 +1,51 @@
 package xyz.korayucar.algorithmstudy.floodingcity;
 
-import xyz.korayucar.algorithmstudy.floodingcity.FloodingCity;
-
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /*
  * @author koray, @date 3/14/17 2:17 PM
  */
 public abstract class AbstractFloodingCityTest {
+
     @Test
     public void testTrivialCase() {
-        int [] city = { 4 , 1,3,4};
+        int[] city = {4, 1, 3, 4};
         assertCapacity(city, 4);
     }
 
+    @Test(expected = NullPointerException.class)
+    public void testNullCheckMade() {
+        createFloodingCity().getWaterCapacity(null);
+    }
+
     @Test
-    public void testAlternation() {
-        int [] city = { 4 , 1,4,1,4,1,4};
+    public void testAlternatingInput() {
+        int[] city = {4, 1, 4, 1, 4, 1, 4};
         assertCapacity(city, 9);
     }
 
     @Test
     public void testBigCity() {
-        int [] city = { 2,6,3,5,2,8,1,4,2,2,5,3,5,7,4,1};
-        assertCapacity(city, 9);
+        int[] city = {2, 6, 3, 5, 2, 8, 1, 4, 2, 2, 5, 3, 5, 7, 4, 1};
+        assertCapacity(city, 35);
     }
 
     @Test(timeout = 3000)
     public void testVeryBigData() {
-        int [] city = new int[100000];
-        for(int i =1 ;i < 100000;i+=2)
+        int[] city = new int[100000];
+        for (int i = 1; i < 100000; i += 2)
             city[i] = 1;
-        assertCapacity(city, 50000);
+        assertCapacity(city, 49999);
     }
 
 
-    abstract FloodingCity createFloodingCity();
 
     private void assertCapacity(int[] city, int expected) {
         int capacity = createFloodingCity().getWaterCapacity(city);
         assertEquals(expected, capacity);
     }
+
+    abstract FloodingCity createFloodingCity();
 }
